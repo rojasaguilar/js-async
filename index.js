5805b94591617ab4f058b37df706ec30d43cd95b
Ramses Rojas <rojas.aguilar.ramses721@outlook.com>
2025-06-27 11:17:42 -0700

    AL NO MANEJAR JS ASYNCRONO, SE TIENEN CALLBACKS DENTRO DE OTROS CALLBACKS

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed:  ${data}`);
 
   superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+    if (err) return console.log(err.message);
     console.log(res.body.message);
 
-    fs.writeFile('dog-img.txt',res.body.message, err => {
-        console.log("dog image save to file");
-    })
+    fs.writeFile('dog-img.txt', res.body.message, (err) => {
+      if (err) return console.log(err.message);
+      console.log('dog image save to file');
+    });
   });
 });
+
+/* EL PROBLEMA CON ESTE CÓDIGO ES QUE, AL NO MANEJAR JS ASYNCRONO, SE TIENEN CALLBACKS DENTRO
+DE OTROS CALLBACKS Y ESO NO ES BUENO*/
